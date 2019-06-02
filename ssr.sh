0cf0e6c00cc2f1008ac2d8f4aa27ad03a1439fbe
coke50306 <51281770+coke50306@users.noreply.github.com>
2019-06-02 14:05:54 +0800

    Update ssr.sh

--- ssr.sh
+++ ssr.sh
@@ -759,9 +759,9 @@ Install_Libsodium(){
 		echo -e "${Info} 安装依赖..."
 		yum -y groupinstall "Development Tools"
 		echo -e "${Info} 下载..."
-		wget  --no-check-certificate -N "https://github.com/jedisct1/libsodium/archive/${Libsodiumr_ver}-RELEASE.tar.gz"
+		wget  --no-check-certificate -N "https://github.com/jedisct1/libsodium/archive/${Libsodiumr_ver}.tar.gz"
 		echo -e "${Info} 解压..."
-		tar -xzf ${Libsodiumr_ver}-RELEASE.tar.gz && cd ${Libsodiumr_ver}-RELEASE
+		tar -xzf ${Libsodiumr_ver}.tar.gz && cd ${Libsodiumr_ver}
 		echo -e "${Info} 编译安装..."
 		./configure --disable-maintainer-mode && make -j2 && make install
 		echo /usr/local/lib > /etc/ld.so.conf.d/usr_local_lib.conf
@@ -770,14 +770,14 @@ Install_Libsodium(){
 		echo -e "${Info} 安装依赖..."
 		apt-get install -y build-essential
 		echo -e "${Info} 下载..."
-		wget  --no-check-certificate -N "https://github.com/jedisct1/libsodium/archive/${Libsodiumr_ver}-RELEASE.tar.gz"
+		wget  --no-check-certificate -N "https://github.com/jedisct1/libsodium/archive/${Libsodiumr_ver}.tar.gz"
 		echo -e "${Info} 解压..."
-		tar -xzf ${Libsodiumr_ver}-RELEASE.tar.gz && cd ${Libsodiumr_ver}-RELEASE
+		tar -xzf ${Libsodiumr_ver}.tar.gz && cd ${Libsodiumr_ver}
 		echo -e "${Info} 编译安装..."
 		./configure --disable-maintainer-mode && make -j2 && make install
 	fi
 	ldconfig
-	cd .. && rm -rf ${Libsodiumr_ver}-RELEASE.tar.gz && rm -rf ${Libsodiumr_ver}-RELEASE
+	cd .. && rm -rf ${Libsodiumr_ver}.tar.gz && rm -rf ${Libsodiumr_ver}
 	[[ ! -e ${Libsodiumr_file} ]] && echo -e "${Error} libsodium 安装失败 !" && exit 1
 	echo && echo -e "${Info} libsodium 安装成功 !" && echo
 }
